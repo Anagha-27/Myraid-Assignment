00e9cea3303b7571bf4a8707b719bb1aca61204d
Anagha-27 <anaghabvanu@gmail.com>
2026-07-02 22:37:28 +0530

    K6 used for load test

---
+++ K6-v0.54.0-windows-amd64/loadtest.js
@@ -0,0 +1,22 @@
+
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '30s', target: 10 },
+    { duration: '1m',  target: 50 },
+    { duration: '30s', target: 100 },
+    { duration: '30s', target: 0 },
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<2000'],
+    http_req_failed: ['rate<0.05'],
+  },
+};
+
+export default function () {
+  const res = http.get('http://13.211.215.91:5000/');
+  check(res, { 'status is 200': (r) => r.status === 200 });
+  sleep(1);
+}
